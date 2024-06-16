3af153dcf6bf51a46c9dacc3befe06317aa9ffd3
maulanaakgn <maulanaazkiassariy@gmail.com>
2024-06-16 14:25:38 +0700

    update article ui, & add article models, article viewmodel

---
+++ weatherapp_v2/lib/viewmodels/viewmodel_disaster_article.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:weatherapp_v2/model/models_disaster_article.dart';
+
+class DisasterArticleViewmodel with ChangeNotifier {
+  bool _isLoading = false;
+  List<News> _disasterArticleList = [];
+
+  bool get isLoading => _isLoading;
+  List<News> get disasterArticleList => _disasterArticleList;
+
+  Future<void> getDisasterArticle() async {
+    _isLoading = true;
+    notifyListeners();
+
+    try {
+      final response = await http.get(Uri.parse("https://api.worldnewsapi.com/search-news?api-key=3082069646fe4f518231429eb29815b0&text=disasters"));
+
+      if (response.statusCode == 200) {
+        _disasterArticleList = disasterArticleFromJson(response.body).news;
+      } else {
+        throw Exception("Failed to load articles");
+      }
+    } catch (e) {
+      print("Error: $e");
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+}
